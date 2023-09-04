de667c63cae382da2e2389547e1da18e4671f2d9
diegomazariegos2002 <3016552860101@ingenieria.usac.edu.gt>
2023-09-03 22:57:18 -0600

    modified profileRoute

--- backend/server_njs/routers/profileRoute.js
+++ backend/server_njs/routers/profileRoute.js
@@ -4,17 +4,8 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 //User CRUD
-router.get('/', userController.findUser); // Obtain a single user
+router.get('/:id', userController.findUser); // Obtain a single user
 router.put('/', userController.updateUser); // Update a user
-
-
-
-router.get('/artistAlbums/:id', adminController.getArtistAlbums); // Obtain all artist albums
-router.get('/artist/:id', adminController.getSingleArtist); // Obtain a single artist
-router.get('/artist', adminController.getAllArtists);   // Obtain all artists
-router.post('/artist', adminController.createArtist);   // Create an artist
-//router.put('/artist', adminController.updateArtist);    // Update an artist
-//router.delete('/artist', adminController.deleteArtist); // Delete an artist
 
 
 module.exports = router;
